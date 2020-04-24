823769d054e621ee8c9cd05b13916aa729325c2d
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-04-24 01:00:43 -0500

    Added AppBard & Grid

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import WelcomeMessage from "./WelcomeMessage";
-// import styled, { css } from "styled-components";
 import AppLayout from "./AppLayout";
+import AppBar from "./AppBar";
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
+        <AppBar />
         <WelcomeMessage />
       </AppLayout>
     );
