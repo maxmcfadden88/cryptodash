b0bc80eb2d0b7038a0f5b9e23ee5b4e321683dd4
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-04-27 20:20:37 -0500

    added CoinGrid

--- src/Shared/Styles.js
+++ src/Shared/Styles.js
@@ -1,5 +1,5 @@
 const theme = "dark";
-//const theme = 'light';
+// const theme = "light";
 export const lightTheme = theme === "light";
 
 export const color = lightTheme ? "white" : "#061a44";
@@ -12,8 +12,8 @@ if (lightTheme) {
 }
 
 export const lightBlueBackground = `background-color: ${color}`;
-export const backgroundColor2 = `background-color: ${color2};`;
-export const greenBackgroundColor = `background-color: ${color3};`;
+export const backgroundColor2 = `background-color: ${color2}`;
+export const greenBackgroundColor = `background-color: ${color3}`;
 
 export const fontColorGreen = `color: #03A9F4`;
 export const fontColorWhite = `color: white`;
@@ -24,8 +24,8 @@ export const greenBoxShadow = `box-shadow: 0px 0px 4px 2px #5fff17`;
 export const redBoxShadow = `box-shadow: 0px 0px 2px 2px #e41111`;
 
 export const fontSizeBig = "font-size: 2em";
-export const fontSize1 = "font-size: 1.5em;";
+export const fontSize1 = "font-size: 1.5em";
 export const fontSize2 = "font-size: 1.0em";
 export const fontSize3 = "font-size: .75em";
 
-export const textAlignCenter = "text-align: center;";
+export const textAlignCenter = "text-align: center";
