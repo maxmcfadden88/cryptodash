450df73034c81bb3943756353995cf61c38adb2d
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-04-28 19:31:09 -0500

    Added fuzzy search and filter

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -18,6 +18,7 @@ export class AppProvider extends React.Component {
       removeCoin: this.removeCoin,
       isInFavorites: this.isInFavorites,
       confirmFavorites: this.confirmFavorites,
+      setFilteredCoins: this.setFilteredCoins,
     };
   }
 
@@ -68,6 +69,8 @@ export class AppProvider extends React.Component {
   }
 
   setPage = (page) => this.setState({ page });
+
+  setFilteredCoins = (filteredCoins) => this.setState({ filteredCoins });
 
   render() {
     return (
