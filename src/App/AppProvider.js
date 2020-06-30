d6dae4a9d5c2a4114a95c69863e5d69e3c8fe3bd
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-06-29 22:59:43 -0700

    Added Dashboard Grid

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -35,6 +35,8 @@ export class AppProvider extends React.Component {
   fetchPrices = async () => {
     if (this.state.firstVisit) return;
     let prices = await this.prices();
+    // We must filter the empty price objects (not in the lecture)
+    prices = prices.filter((price) => Object.keys(price).length);
     this.setState({ prices });
   };
 
