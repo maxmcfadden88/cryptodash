cece198fe01aa2c7a3404934c7d617d527433f5c
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-07-02 19:53:00 -0700

    Added CoinSpotlight

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,19 +1,24 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 import Page from "../Shared/Page";
 import PriceGrid from "./PriceGrid";
+import CoinSpotLight from "./CoinSpotLight";
 
-// const ChartGrid = styled.div`
-//   display: grid;
-//   margin-top: 20px;
-//   grid-gap: 15px;
-//   grid-template-columns: 1fr 3fr;
-// `;
+const ChartGrid = styled.div`
+  display: grid;
+  margin-top: 20px;
+  grid-gap: 15px;
+  grid-template-columns: 1fr 3fr;
+`;
 
 export default function () {
   return (
     <Page name="dashboard">
       <PriceGrid />
+      <ChartGrid>
+        <CoinSpotLight />
+        <div>Chart Goes here</div>
+      </ChartGrid>
     </Page>
   );
 }
