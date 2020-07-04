a80fdc87dda7f0d55ce295d8483a12606da9f2a0
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-07-03 19:01:21 -0700

    added Historical data

--- src/Dashboard/HighchartsConfig.js
+++ src/Dashboard/HighchartsConfig.js
@@ -1,4 +1,4 @@
-export default function () {
+export default function (historical) {
   return {
     title: {
       text: "",
@@ -11,9 +11,7 @@ export default function () {
     },
 
     xAxis: {
-      accessibility: {
-        rangeDescription: "Range: 2010 to 2017",
-      },
+      type: "datetime",
     },
 
     legend: {
@@ -31,12 +29,7 @@ export default function () {
       },
     },
 
-    series: [
-      {
-        name: "Installation",
-        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
-      },
-    ],
+    series: historical,
 
     responsive: {
       rules: [
